be04da7118ee59a10af05e164eb6c66bb02f2a16
Marçal <matheuscpm28@gmail.com>
2026-01-14 15:05:39 -0300

    rollback prisma to version 5 and implement prisma service with prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PricesModule } from './prices/prices.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), PricesModule,],
+  imports: [ScheduleModule.forRoot(), PricesModule, PrismaModule,],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prices/prices.service.ts
+++ src/prices/prices.service.ts
@@ -1,23 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import axios from 'axios';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PricesService {
-    @Cron(CronExpression.EVERY_MINUTE)
-async collectPrices() {
-    console.log('Collecting prices...');
-    const response = await axios.get(
-      'https://api.coingecko.com/api/v3/simple/price',
-      {
-        params: {
-          ids: 'bitcoin,ethereum',
-          vs_currencies: 'usd',
-        },
-      }
-    );
-    console.log(response.data);
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
-}
-
-
