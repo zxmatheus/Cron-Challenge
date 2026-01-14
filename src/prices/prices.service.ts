9ca9620d97c59a9684659b992b79a693bac1ee32
Marçal <matheuscpm28@gmail.com>
2026-01-14 13:50:20 -0300

    import basic api

--- src/prices/prices.service.ts
+++ src/prices/prices.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Injectable()
 export class PricesService {
     @Cron(CronExpression.EVERY_MINUTE)
 async collectPrices() {
-  console.log('Collecting prices...');
+    console.log('Collecting prices...');
+    const response = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price',
+      {
+        params: {
+          ids: 'bitcoin,ethereum',
+          vs_currencies: 'usd',
+        },
+      }
+    );
+    console.log(response.data);
 }
 }
 
