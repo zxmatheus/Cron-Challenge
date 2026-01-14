ea12fd80a94b10f0bd9a39eace59b325dd55c6a0
Marçal <matheuscpm28@gmail.com>
2026-01-14 16:59:11 -0300

    inject prisma into service and implement error

--- src/prices/prices.module.ts
+++ src/prices/prices.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PricesService } from './prices.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  providers: [PricesService]
+  imports: [PrismaModule],
+  providers: [PricesService],
 })
 export class PricesModule {}
--- src/prices/prices.service.ts
+++ src/prices/prices.service.ts
@@ -1,12 +1,37 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit
-{
-  async onModuleInit() {
-    await this.$connect();
+export class PricesService {
+  constructor(private prisma: PrismaService) {}
+
+  @Cron(CronExpression.EVERY_5_MINUTES)
+  async collectPrices() {
+    console.log('Collecting prices...');
+
+    const { data } = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price',
+      {
+        params: {
+          ids: 'bitcoin,ethereum',
+          vs_currencies: 'usd',
+        },
+      },
+    );
+
+    console.log(data);
+    } catch (error: any) {
+  if (error.response?.status === 429) {
+    console.warn('Rate limit atingido, pulando execução');
+    return;
   }
+
+  console.error('Erro ao buscar preços:', error.message);
 }
+
+    
+
+  }
+
